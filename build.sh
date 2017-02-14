7b54cd3a8fa6310bb60d0cd2e9866b7ab19ebb72
Joshua Higgins <joshiggins@gmail.com>
2017-02-14 16:54:53 +0000

    use busybox init for now, eugh

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ rm dev/console
 mknod -m 622 dev/console c 5 1
 mknod -m 622 dev/tty0 c 4 0
 mv vmlinuz ..
-find | ( set -x; cpio -o -H newc | xz -9 --format=lzma --verbose --verbose ) > ../initramfs.xz
+find | ( set -x; cpio -o -H newc | xz -1 --format=lzma --verbose --verbose ) > ../initramfs.xz
 cd ..
 rm -rf extract-fs
 rm fs.tar
